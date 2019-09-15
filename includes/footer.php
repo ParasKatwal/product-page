fc0af47f1d42fb58608e887ed3a1be9bd955768a
ParasKatwal <katwal.paras@gmail.com>
2019-09-15 13:19:59 +0545

    form section updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,7 @@
 
-
+<script id="__bs_script__">//<![CDATA[
+    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.26.7'><\/script>".replace("HOST", location.hostname));
+//]]></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>     
 <script src="assets_front/js/bootstrap.min.js"></script>       
 <script src="assets_front/js/custom.js"></script>
--- index.php
+++ index.php
@@ -17,184 +17,167 @@
 
 <section class="product">
     <div class="container">
-        <div class="row">
-            <div class="col-sm-12 col-md-8 slider slider-single">
-                <div class="slider-single_wrap">
-                    <img src="assets_front/img/product3.png" alt="">
-                    <div class="details">
-                        <h5>SKYWORTH MODEL 2120</h5>
-                        <p><span>Resolution</span> <span>FHD</span></p>
-                        <p><span>Category</span> <span>Google Andriod TV</span></p>
-                        <p><span>Model</span> <span>2120</span></p>
-                        <p><span>Television</span> <span>Goole Andriod 4K</span></p>
-                        <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
-                    </div>
-                </div>
-                <div class="slider-single_wrap">
-                    <img src="assets_front/img/product4.png" alt="">
-                    <div class="details">
-                        <h5>SKYWORTH MODEL 2120</h5>
-                        <p><span>Resolution</span> <span>FHD</span></p>
-                        <p><span>Category</span> <span>Google Andriod TV</span></p>
-                        <p><span>Model</span> <span>2120</span></p>
-                        <p><span>Television</span> <span>Goole Andriod 4K</span></p>
-                        <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
-                    </div>
-                </div>
-                <div class="slider-single_wrap">
-                    <img src="assets_front/img/product3.png" alt="">
-                    <div class="details">
-                        <h5>SKYWORTH MODEL 2120</h5>
-                        <p><span>Resolution</span> <span>FHD</span></p>
-                        <p><span>Category</span> <span>Google Andriod TV</span></p>
-                        <p><span>Model</span> <span>2120</span></p>
-                        <p><span>Television</span> <span>Goole Andriod 4K</span></p>
-                        <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
-                    </div>
-                </div>
-                <div class="slider-single_wrap">
-                    <img src="assets_front/img/product4.png" alt="">
-                    <div class="details">
-                        <h5>SKYWORTH MODEL 2120</h5>
-                        <p><span>Resolution</span> <span>FHD</span></p>
-                        <p><span>Category</span> <span>Google Andriod TV</span></p>
-                        <p><span>Model</span> <span>2120</span></p>
-                        <p><span>Television</span> <span>Goole Andriod 4K</span></p>
-                        <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
-                    </div>
-                </div>
-                <div class="slider-single_wrap">
-                    <img src="assets_front/img/product3.png" alt="">
-                    <div class="details">
-                        <h5>SKYWORTH MODEL 2120</h5>
-                        <p><span>Resolution</span> <span>FHD</span></p>
-                        <p><span>Category</span> <span>Google Andriod TV</span></p>
-                        <p><span>Model</span> <span>2120</span></p>
-                        <p><span>Television</span> <span>Goole Andriod 4K</span></p>
-                        <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
-                    </div>
-                </div>
-                <div class="slider-single_wrap">
-                    <img src="assets_front/img/product4.png" alt="">
-                    <div class="details">
-                        <h5>SKYWORTH MODEL 2120</h5>
-                        <p><span>Resolution</span> <span>FHD</span></p>
-                        <p><span>Category</span> <span>Google Andriod TV</span></p>
-                        <p><span>Model</span> <span>2120</span></p>
-                        <p><span>Television</span> <span>Goole Andriod 4K</span></p>
-                        <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
-                    </div>
-                </div>
-                <div class="slider-single_wrap">
-                    <img src="assets_front/img/product3.png" alt="">
-                    <div class="details">
-                        <h5>SKYWORTH MODEL 2120</h5>
-                        <p><span>Resolution</span> <span>FHD</span></p>
-                        <p><span>Category</span> <span>Google Andriod TV</span></p>
-                        <p><span>Model</span> <span>2120</span></p>
-                        <p><span>Television</span> <span>Goole Andriod 4K</span></p>
-                        <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
-                    </div>
-                </div>
-                <div class="slider-single_wrap">
-                    <img src="assets_front/img/product4.png" alt="">
-                    <div class="details">
-                        <h5>SKYWORTH MODEL 2120</h5>
-                        <p><span>Resolution</span> <span>FHD</span></p>
-                        <p><span>Category</span> <span>Google Andriod TV</span></p>
-                        <p><span>Model</span> <span>2120</span></p>
-                        <p><span>Television</span> <span>Goole Andriod 4K</span></p>
-                        <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
-                    </div>
+        <div class="slider slider-single">
+            <div class="slider-single_wrap">
+                <img src="assets_front/img/product3.png" alt="">
+                <div class="details">
+                    <h5>SKYWORTH MODEL 2120</h5>
+                    <p><span>Resolution</span> <span>FHD</span></p>
+                    <p><span>Category</span> <span>Google Andriod TV</span></p>
+                    <p><span>Model</span> <span>2120</span></p>
+                    <p><span>Television</span> <span>Goole Andriod 4K</span></p>
+                    <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
                 </div>
             </div>
-            <div class="col-md-4 enquiry">
-                <form>
-                    <h6 class="text-center">Get In Touch With Us</h6>
-                    <div class="form-group">
-                        <input type="text" class="form-control"  placeholder="Enter Your Name">
-                    </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control"  placeholder="Enter Your Email">
-                    </div>
-                    <div class="form-group">
-                        <textarea class="form-control" placeholder="Enquiry" rows="3"></textarea>
-                    </div>
-                    <button type="submit" class="btn">SUBMIT</button>
-                </form>
-            </div>
-            <div class="col-sm-12 slider slider-nav">
-                <div class="item_wrap is-active">
-                    <div class="item">
-                        <h6>Skyworth EU20</h6>
-                        <img src="assets_front/img/item.png" alt="">
-                        <p>NRS 42,000</p>
-                    </div>
-                </div>
-                <div class="item_wrap">
-                    <div class="item">
-                        <h6>Skyworth EU20</h6>
-                        <img src="assets_front/img/item.png" alt="">
-                        <p>NRS 42,000</p>
-                    </div>
-                </div><div class="item_wrap">
-                    <div class="item">
-                        <h6>Skyworth EU20</h6>
-                        <img src="assets_front/img/item.png" alt="">
-                        <p>NRS 42,000</p>
-                    </div>
-                </div><div class="item_wrap">
-                    <div class="item">
-                        <h6>Skyworth EU20</h6>
-                        <img src="assets_front/img/item.png" alt="">
-                        <p>NRS 42,000</p>
-                    </div>
-                </div><div class="item_wrap">
-                    <div class="item">
-                        <h6>Skyworth EU20</h6>
-                        <img src="assets_front/img/item.png" alt="">
-                        <p>NRS 42,000</p>
-                    </div>
-                </div><div class="item_wrap">
-                    <div class="item">
-                        <h6>Skyworth EU20</h6>
-                        <img src="assets_front/img/item.png" alt="">
-                        <p>NRS 42,000</p>
-                    </div>
-                </div><div class="item_wrap">
-                    <div class="item">
-                        <h6>Skyworth EU20</h6>
-                        <img src="assets_front/img/item.png" alt="">
-                        <p>NRS 42,000</p>
-                    </div>
-                </div><div class="item_wrap">
-                    <div class="item">
-                        <h6>Skyworth EU20</h6>
-                        <img src="assets_front/img/item.png" alt="">
-                        <p>NRS 42,000</p>
-                    </div>
-                </div><div class="item_wrap">
-                    <div class="item">
-                        <h6>Skyworth EU20</h6>
-                        <img src="assets_front/img/item.png" alt="">
-                        <p>NRS 42,000</p>
-                    </div>
+            <div class="slider-single_wrap">
+                <img src="assets_front/img/product4.png" alt="">
+                <div class="details">
+                    <h5>SKYWORTH MODEL 2120</h5>
+                    <p><span>Resolution</span> <span>FHD</span></p>
+                    <p><span>Category</span> <span>Google Andriod TV</span></p>
+                    <p><span>Model</span> <span>2120</span></p>
+                    <p><span>Television</span> <span>Goole Andriod 4K</span></p>
+                    <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
                 </div>
             </div>
-            <div class="col-sm-12 enquiry_mobileview">
-                <form>
-                    <h6 class="text-center">Get In Touch With Us</h6>
-                    <div class="form-group">
-                        <input type="text" class="form-control"  placeholder="Enter Your Name">
-                    </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control"  placeholder="Enter Your Email">
-                    </div>
-                    <div class="form-group">
-                        <textarea class="form-control" placeholder="Enquiry" rows="3"></textarea>
-                    </div>
-                    <button type="submit" class="btn">SUBMIT</button>
-                </form>
+            <div class="slider-single_wrap">
+                <img src="assets_front/img/product3.png" alt="">
+                <div class="details">
+                    <h5>SKYWORTH MODEL 2120</h5>
+                    <p><span>Resolution</span> <span>FHD</span></p>
+                    <p><span>Category</span> <span>Google Andriod TV</span></p>
+                    <p><span>Model</span> <span>2120</span></p>
+                    <p><span>Television</span> <span>Goole Andriod 4K</span></p>
+                    <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
+                </div>
+            </div>
+            <div class="slider-single_wrap">
+                <img src="assets_front/img/product4.png" alt="">
+                <div class="details">
+                    <h5>SKYWORTH MODEL 2120</h5>
+                    <p><span>Resolution</span> <span>FHD</span></p>
+                    <p><span>Category</span> <span>Google Andriod TV</span></p>
+                    <p><span>Model</span> <span>2120</span></p>
+                    <p><span>Television</span> <span>Goole Andriod 4K</span></p>
+                    <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
+                </div>
+            </div>
+            <div class="slider-single_wrap">
+                <img src="assets_front/img/product3.png" alt="">
+                <div class="details">
+                    <h5>SKYWORTH MODEL 2120</h5>
+                    <p><span>Resolution</span> <span>FHD</span></p>
+                    <p><span>Category</span> <span>Google Andriod TV</span></p>
+                    <p><span>Model</span> <span>2120</span></p>
+                    <p><span>Television</span> <span>Goole Andriod 4K</span></p>
+                    <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
+                </div>
+            </div>
+            <div class="slider-single_wrap">
+                <img src="assets_front/img/product4.png" alt="">
+                <div class="details">
+                    <h5>SKYWORTH MODEL 2120</h5>
+                    <p><span>Resolution</span> <span>FHD</span></p>
+                    <p><span>Category</span> <span>Google Andriod TV</span></p>
+                    <p><span>Model</span> <span>2120</span></p>
+                    <p><span>Television</span> <span>Goole Andriod 4K</span></p>
+                    <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
+                </div>
+            </div>
+            <div class="slider-single_wrap">
+                <img src="assets_front/img/product3.png" alt="">
+                <div class="details">
+                    <h5>SKYWORTH MODEL 2120</h5>
+                    <p><span>Resolution</span> <span>FHD</span></p>
+                    <p><span>Category</span> <span>Google Andriod TV</span></p>
+                    <p><span>Model</span> <span>2120</span></p>
+                    <p><span>Television</span> <span>Goole Andriod 4K</span></p>
+                    <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
+                </div>
+            </div>
+            <div class="slider-single_wrap">
+                <img src="assets_front/img/product4.png" alt="">
+                <div class="details">
+                    <h5>SKYWORTH MODEL 2120</h5>
+                    <p><span>Resolution</span> <span>FHD</span></p>
+                    <p><span>Category</span> <span>Google Andriod TV</span></p>
+                    <p><span>Model</span> <span>2120</span></p>
+                    <p><span>Television</span> <span>Goole Andriod 4K</span></p>
+                    <p class="mt-auto"><span>MRP</span> <span>NRS 40,300</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="enquiry">
+            <form>
+                <h6 class="text-center">Get In Touch With Us</h6>
+                <div class="form-group">
+                    <input type="text" class="form-control"  placeholder="Enter Your Name">
+                </div>
+                <div class="form-group">
+                    <input type="email" class="form-control"  placeholder="Enter Your Email">
+                </div>
+                <div class="form-group">
+                    <textarea class="form-control" placeholder="Enquiry" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn">SUBMIT</button>
+            </form>
+        </div>
+        <div class="slider slider-nav">
+            <div class="item_wrap is-active">
+                <div class="item">
+                    <h6>Skyworth EU20</h6>
+                    <img src="assets_front/img/item.png" alt="">
+                    <p>NRS 42,000</p>
+                </div>
+            </div>
+            <div class="item_wrap">
+                <div class="item">
+                    <h6>Skyworth EU20</h6>
+                    <img src="assets_front/img/item.png" alt="">
+                    <p>NRS 42,000</p>
+                </div>
+            </div><div class="item_wrap">
+                <div class="item">
+                    <h6>Skyworth EU20</h6>
+                    <img src="assets_front/img/item.png" alt="">
+                    <p>NRS 42,000</p>
+                </div>
+            </div><div class="item_wrap">
+                <div class="item">
+                    <h6>Skyworth EU20</h6>
+                    <img src="assets_front/img/item.png" alt="">
+                    <p>NRS 42,000</p>
+                </div>
+            </div><div class="item_wrap">
+                <div class="item">
+                    <h6>Skyworth EU20</h6>
+                    <img src="assets_front/img/item.png" alt="">
+                    <p>NRS 42,000</p>
+                </div>
+            </div><div class="item_wrap">
+                <div class="item">
+                    <h6>Skyworth EU20</h6>
+                    <img src="assets_front/img/item.png" alt="">
+                    <p>NRS 42,000</p>
+                </div>
+            </div><div class="item_wrap">
+                <div class="item">
+                    <h6>Skyworth EU20</h6>
+                    <img src="assets_front/img/item.png" alt="">
+                    <p>NRS 42,000</p>
+                </div>
+            </div><div class="item_wrap">
+                <div class="item">
+                    <h6>Skyworth EU20</h6>
+                    <img src="assets_front/img/item.png" alt="">
+                    <p>NRS 42,000</p>
+                </div>
+            </div><div class="item_wrap">
+                <div class="item">
+                    <h6>Skyworth EU20</h6>
+                    <img src="assets_front/img/item.png" alt="">
+                    <p>NRS 42,000</p>
+                </div>
             </div>
         </div>
     </div>
